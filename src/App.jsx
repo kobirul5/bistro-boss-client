5a05d790062fa8eba79769d2b4abd117a00e504f
kobirul5 <kobirul05j@gmail.com>
2024-12-29 00:16:03 +0600

    added footer section and routers

--- src/App.jsx
+++ src/App.jsx
@@ -9,10 +9,6 @@ function App() {
   return (
     <>
       <h1>Vite + React</h1>
-      <button className="btn btn-outline">Default</button>
-<button className="btn btn-outline btn-primary">Primary</button>
-<button className="btn btn-outline btn-secondary">Secondary</button>
-<button className="btn btn-outline btn-accent">Accent</button>
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { RouterProvider } from 'react-router-dom'
+import { router } from './Routs/Routs.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <div className='max-w-screen-2xl mx-auto'>
+      <RouterProvider router={router} />
+    </div>
   </StrictMode>,
 )
