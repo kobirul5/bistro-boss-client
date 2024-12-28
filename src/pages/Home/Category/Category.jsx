31ce3a8f765aac4fd7250a0abe867a99b1396668
kobirul5 <kobirul05j@gmail.com>
2024-12-29 01:18:14 +0600

    added category section

---
+++ src/pages/Home/Category/Category.jsx
@@ -0,0 +1,51 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+import 'swiper/css';
+import 'swiper/css/pagination';
+import { Pagination } from 'swiper/modules';
+
+import Slide1 from '../../../assets/home/slide1.jpg'
+import Slide2 from '../../../assets/home/slide2.jpg'
+import Slide3 from '../../../assets/home/slide3.jpg'
+import Slide4 from '../../../assets/home/slide4.jpg'
+import Slide5 from '../../../assets/home/slide5.jpg'
+
+
+
+const Category = () => {
+    return (
+        <Swiper
+            slidesPerView={4}
+            spaceBetween={30}
+            centeredSlides={true}
+            pagination={{
+                clickable: true,
+            }}
+            modules={[Pagination]}
+            className="mySwiper mb-24"
+        >
+            <SwiperSlide>
+                <img src={Slide1} alt="" />
+                <h2 className='text-4xl uppercase -mt-16 text-white text-center'>salad</h2>
+            </SwiperSlide>
+            <SwiperSlide>
+                <img src={Slide2} alt="" />
+                <h2 className='text-4xl uppercase -mt-16 text-white text-center'>salad</h2>
+            </SwiperSlide>
+            <SwiperSlide>
+                <img src={Slide3} alt="" />
+                <h2 className='text-4xl uppercase -mt-16 text-white text-center'>salad</h2>
+            </SwiperSlide>
+            <SwiperSlide>
+                <img src={Slide4} alt="" />
+                <h2 className='text-4xl uppercase -mt-16 text-white text-center'>salad</h2>
+            </SwiperSlide>
+            <SwiperSlide>
+                <img src={Slide5} alt="" />
+                <h2 className='text-4xl uppercase -mt-16 text-white text-center'>salad</h2>
+            </SwiperSlide>
+        </Swiper>
+    );
+};
+
+export default Category;
--- src/pages/Shared/Navbar/Navbar.jsx
+++ src/pages/Shared/Navbar/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
     </>
 
     return (
-        <div className="navbar bg-base-100">
+        <div className="navbar fixed z-10 text-white bg-opacity-20 bg-black">
             <div className="navbar-start">
                 <div className="dropdown">
                     <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
