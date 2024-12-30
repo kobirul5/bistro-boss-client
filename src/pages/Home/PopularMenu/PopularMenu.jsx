35d5a279a83e892f14e6d459fd2f43a200d5e562
kobirul5 <kobirul05j@gmail.com>
2024-12-30 11:39:15 +0600

    added custom hook for manu items

--- src/pages/Home/PopularMenu/PopularMenu.jsx
+++ src/pages/Home/PopularMenu/PopularMenu.jsx
@@ -1,17 +1,23 @@
 import { useEffect, useState } from "react";
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
 import PopularCard from "../../../components/PopulerCard/PopularCard";
+import useMenu from "../../../hooks/useMenu";
 
 const PopularMenu = () => {
-    const [menu, setMenu] = useState([])
-    useEffect(()=>{
-        fetch("menu.json")
-        .then(res=>res.json())
-        .then(data=>{
-            const popularItems= data.filter(item=>item.category === "popular")
-            setMenu(popularItems)
-        })
-    },[])
+    const [menu] = useMenu()
+    const popular= menu.filter(item=>item.category === "popular")
+
+
+
+    // const [menu, setMenu] = useState([])
+    // useEffect(()=>{
+    //     fetch("menu.json")
+    //     .then(res=>res.json())
+    //     .then(data=>{
+    //         const popularItems= data.filter(item=>item.category === "popular")
+    //         setMenu(popularItems)
+    //     })
+    // },[])
     return (
         <section className="px-4 mb-24">
             <SectionTitle
@@ -20,7 +26,7 @@ const PopularMenu = () => {
             ></SectionTitle>
             <div className="grid grid-cols-2 gap-5">
                 {
-                    menu.map((item,idx)=><PopularCard
+                    popular.map((item,idx)=><PopularCard
                     key={idx}
                     item={item}
                     ></PopularCard>)
