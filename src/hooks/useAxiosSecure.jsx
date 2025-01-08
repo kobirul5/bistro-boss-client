8dc88b56d96a8ede8d3fef2ca8141d1aa09032f5
kobirul5 <kobirul05j@gmail.com>
2025-01-09 02:26:47 +0600

    added interceptors

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -3,6 +3,26 @@ import axios from 'axios';
     baseURL: 'http://localhost:5000'
 })
 const useAxiosSecure = () => {
+    axiosSecure.interceptors.request.use(function (config) {
+        const token = localStorage.getItem("access-token")
+        console.log("request stop by interceptors", token)
+        config.headers.authorization= `bearer ${token}`;
+        return config;
+      }, function (error) {
+        // Do something with request error
+        return Promise.reject(error);
+      });
+
+      // erroor
+      axiosSecure.interceptors.response.use(function (response) {
+        return response;
+      }, function (error) {
+        const status = error.response.status;
+        console.log(status)
+        return Promise.reject(error);
+      });
+
+
     return axiosSecure;
 };
 
--- src/pages/Dashboard/AllUsers/AllUsers.jsx
+++ src/pages/Dashboard/AllUsers/AllUsers.jsx
@@ -1,16 +1,58 @@
 import { useQuery } from "@tanstack/react-query";
 import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import { FaTrashAlt, FaUsers } from "react-icons/fa";
+import Swal from "sweetalert2";
 
 
 const AllUsers = () => {
     const axiosSecure = useAxiosSecure();
-    const { data: users = [] } = useQuery({
+    const { data: users = [], refetch } = useQuery({
         queryKey: ['users'],
         queryFn: async () => {
             const res = await axiosSecure.get('/users');
             return res.data;
         }
     })
+
+    const handleMakeAdmin = (user) => {
+        axiosSecure.patch(`/users/admin/${user._id}`)
+            .then(res => {
+                if (res.data.modifiedCount > 0) {
+                    Swal.fire({
+                        title: "success",
+                        text: `${user.name} has been Admin now`,
+                        icon: "success"
+                    });
+                    refetch();
+                }
+            })
+    }
+
+    const handleDeleteUser = (user) => {
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                axiosSecure.delete(`/users/${user._id}`)
+                    .then(res => {
+                        if (res.data.deletedCount > 0) {
+                            Swal.fire({
+                                title: "Deleted!",
+                                text: "Your file has been deleted.",
+                                icon: "success"
+                            });
+                            refetch();
+                        }
+                    })
+            }
+        });
+    }
     return (
         <div>
             <div className="flex justify-evenly my-4">
@@ -31,15 +73,27 @@ const AllUsers = () => {
                     </thead>
                     <tbody>
                         {
-                            users?.map((user,idx)=><tr key={user._id}>
-                                <th>{idx+1}</th>
+                            users?.map((user, idx) => <tr key={user._id}>
+                                <th>{idx + 1}</th>
                                 <td>{user.name}</td>
                                 <td>{user.email}</td>
-                                <td>Blue</td>
-                                <td>Blue</td>
+                                <td>
+                                    {
+                                        user?.role == "admin" ? "admin":
+
+                                        <button onClick={() => handleMakeAdmin(user)} className="btn btn-sm btn-outline  text-2xl bg-dashboard-primary border-dashboard-primary text-white">
+                                            <FaUsers />
+                                        </button>
+                                    }
+                                </td>
+                                <td>
+                                    <button onClick={() => handleDeleteUser(user)} className="btn btn-sm btn-outline text-2xl btn-error">
+                                        <FaTrashAlt />
+                                    </button>
+                                </td>
                             </tr>)
                         }
-                        
+
                     </tbody>
                 </table>
             </div>
